9904783046f4b7d285202aa7b6ea9eae3b038eff
jhcho <jjhbaram@naver.com>
2023-03-16 00:20:31 +0900

    first commit

---
+++ 2048/view.js
@@ -0,0 +1,48 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth - 100;
+canvas.height = window.innerHeight - 100;
+
+var font = '25px serif';
+
+var boxSize = 50;
+
+class Square {
+    constructor(){
+        this.num = 2;
+        this.width = boxSize;
+        this.height = boxSize;
+    }
+
+    draw(){
+
+        ctx.fillStyle = '#ff0000';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+
+        ctx.fillStyle = 'black';
+        ctx.font = font;
+        ctx.fillText(this.num, this.x + this.width / 3, this.y + this.height / 3 * 2);
+    }
+}
+
+var square = [];
+for(var y = 0; y < 4; y++)
+{
+    for(var x = 0; x < 4; x++)
+    {
+        var s = new Square();
+        s.y = 50 + y * (boxSize + 10);
+        s.x = 50 + x * (boxSize + 10);
+
+        square.push(s);
+    }
+}
+
+square.forEach(a => { a.draw() });
+
+ctx.fillStyle = 'black';
+ctx.font = font;
+ctx.fillText('SCORE : ', 20, 20);
+
+
